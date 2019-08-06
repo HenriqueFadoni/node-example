db2884a83d17e279fab27ff958b9aa182b2e248a
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-08-06 20:38:15 -0300

    Adding express and routes to the project

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
-    res.send(`
-        <form action="/product" method="POST">
-            <input type="text" name="title"/>
-            <button type="submit">ADD PRODUCT</button>
-        </form>`
-    );
+    res.sendFile(path.join(__dirname, '../', 'view', 'add-product.html'));
 });
 
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
     console.log(req.body);
     res.redirect('/');
 });
