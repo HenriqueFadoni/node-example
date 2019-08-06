053ed6f6629886c6633e4e31ab04e6a330ffa39c
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-08-06 20:54:28 -0300

    Bug Fix - Routes

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(rootDir, 'view', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
