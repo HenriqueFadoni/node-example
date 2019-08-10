ab2c9dc361faa1c41ad5e1d98c9b6a782d93f4b3
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-08-10 19:27:29 -0300

    Adding Pug to the Project

--- routes/admin.js
+++ routes/admin.js
@@ -5,14 +5,18 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const products = [];
 
 router.get('/add-product', (req, res) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
+    products.push({
+        title: req.body.title
+    });
+    res.redirect('/admin');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
