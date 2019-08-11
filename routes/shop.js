361cc72bd16a3e8883cba73a8e21ef2a12e1053b
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-08-10 21:38:26 -0300

    Adding Product Items to Shop page

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('shop');
+    const products = adminData.products;
+    res.render('shop', { prods: products });
     // console.log(adminData.products);
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
