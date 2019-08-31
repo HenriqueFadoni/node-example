3d6f9d83626652e6e4f701d1309349eb9522f2fb
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-08-31 13:09:29 -0300

    Adding Items to the Cart and Product page

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,8 +12,13 @@ exports.getProducts = (req, res) => {
 
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
-    console.log(prodId);
-    res.redirect('/');
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: product.title,
+            path: '/products'
+        });
+    });
 }
 
 exports.getIndex = (req, res, next) => {
@@ -32,6 +37,11 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart'
     });
 };
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    res.redirect('/cart', shopController.postCart);
+}
 
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
