44e6f394ffd8803232daab8e4ef6795df3340117
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-08-31 19:44:22 -0300

    Creating a Product Page from data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res) => {
     Product.fetchAll(products => {
@@ -40,7 +41,10 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    res.redirect('/cart', shopController.postCart);
+    Product.findById(prodId, product => {
+        Cart.addProduct(prodId, product.price);
+    });
+    res.redirect('/cart');
 }
 
 exports.getOrders = (req, res, next) => {
