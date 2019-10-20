76c5fd43d5023d952347792deedd3b8d29ad6003
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-10-20 17:12:01 -0300

    Editing The Product Data

--- models/product.js
+++ models/product.js
@@ -3,49 +3,61 @@ const rootDir = require('../util/path');
 const path = require('path');
 
 const productsFile = path.join(
-    rootDir,
-    'data',
-    'products.json'
+  rootDir,
+  'data',
+  'products.json'
 );
 
 const getProductsFromFile = (callBack) => {
-    fs.readFile(productsFile, (err, fileContent) => {
-        if (err) {
-            callBack([]);
-        } else {
-            callBack(JSON.parse(fileContent));
-        }
-    });
+  fs.readFile(productsFile, (err, fileContent) => {
+    if (err) {
+      callBack([]);
+    } else {
+      callBack(JSON.parse(fileContent));
+    }
+  });
 }
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-    }
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    // Adding Products to the Document
-    save() {
+  // Adding Products to the Document
+  save() {
+    getProductsFromFile(products => {
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(productsFile, JSON.stringify(updatedProducts), err => {
+          console.log(err)
+        });
+      } else {
         this.id = Math.random().toString();
-        getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(productsFile, JSON.stringify(products), err => {
-                console.log(err)
-            });
+        products.push(this);
+        fs.writeFile(productsFile, JSON.stringify(products), err => {
+          console.log(err)
         });
-    }
+      }
+    });
+  }
 
-    // Fetch All Data from the File
-    static fetchAll(callBack) {
-        getProductsFromFile(callBack);
-    }
+  // Fetch All Data from the File
+  static fetchAll(callBack) {
+    getProductsFromFile(callBack);
+  }
 
-    static findById(id, cb) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === id);
-            cb(product);
-        });
-    }
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
 }
