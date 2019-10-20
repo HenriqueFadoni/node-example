3ba5db5625fd9ce4abd620ce6b8520e3b4f7f532
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-10-20 19:56:13 -0300

    Deleting Cart Items

--- models/product.js
+++ models/product.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const rootDir = require('../util/path');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const productsFile = path.join(
   rootDir,
   'data',
@@ -46,6 +48,18 @@ module.exports = class Product {
           console.log(err)
         });
       }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id === id);
+      fs.writeFile(productsFile, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
     });
   }
 
